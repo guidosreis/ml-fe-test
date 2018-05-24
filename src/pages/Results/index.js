a5bac8a083b77cc11a45408257473a6a6a697953
Guilherme dos Reis <greis.gnr@gmail.com>
2018-05-24 09:01:32 -0300

    Build initial structure of Results page component

--- src/pages/Results/index.js
+++ src/pages/Results/index.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react'
+import queryString from 'query-string'
+import axios from 'axios'
 
 export class Results extends Component {
+  constructor(props) {
+    super(props)
+
+    const parsed = queryString.parse(props.location.search)
+    this.query = parsed.q
+    if (!this.query) {
+      // alert user that query is empty
+    }
+
+    this.state = {
+      products: []
+    }
+
+    this.getProducts()
+  }
+
+  getProducts() {
+    const url = `https://api.mercadolibre.com/sites/MLA/search?q=${this.query}`
+
+    axios.get(url).then(response => {
+      const products = response.data.results.slice(0, 4)
+      console.log('products: ', products)
+      this.setState({products})
+    })
+  }
+
   render() {
     return (
-      <h1>Results page</h1>
+      <React.Fragment>
+        <h1>Results page</h1>
+        {
+          !!this.state.products.length &&
+          this.state.products.map(product => (
+            <div key={product.id}>
+              {product.title}
+            </div>
+          ))
+        }
+      </React.Fragment>
     )
   }
 }
