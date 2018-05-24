439bedd1f429440e4c33999a9d2c4103de84df88
Guilherme dos Reis <greis.gnr@gmail.com>
2018-05-24 09:01:15 -0300

    Build initial structure of Header component

--- src/core/Header/index.js
+++ src/core/Header/index.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 
 export class Header extends Component {
+  constructor(props) {
+    super(props)
+
+    this.handleSearch = this.handleSearch.bind(this)
+
+    this.state = {
+      redirect: false
+    }
+  }
+
+  handleSearch(event) {
+    event.preventDefault()
+    const q = this.refs.searchInput.value
+
+    this.setState({
+      redirect: `results?q=${q}`
+    })
+  }
+
   render() {
     return (
-      <h1>Header component</h1>
+      <form className="search-bar" onSubmit={this.handleSearch}>
+        <input
+          type="seach"
+          className="seach-bar__input"
+          placeholder="Nunca deixe de buscar"
+          ref="searchInput"
+        />
+        <button type="submit" className="search-bar__button">
+          <i className="fa fa-search" />
+        </button>
+        {
+          this.state.redirect &&
+          <Redirect push to={this.state.redirect} />
+        }
+      </form>
     )
   }
 }
