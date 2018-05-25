ad09c97d854153e675202cf2615b75d44e610962
Guilherme dos Reis <greis.gnr@gmail.com>
2018-05-25 09:28:21 -0300

    Define Product component html

--- src/core/ProductList/Product/index.js
+++ src/core/ProductList/Product/index.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Product = () => {
-  return (
-    <h1>Product</h1>
-  )
+import formatPrice from '../../../utils/formatPrice'
+
+const Product = ({ product }) => (
+  <article className="product">
+    <figure className="product__thumb">
+      <img src={product.thumbnail} alt={product.title} />
+    </figure>
+    <div className="product__info">
+      <span className="product__info__price">
+        $ {formatPrice(product.price)}
+      </span>
+      <p className="product__info__title">
+        {product.title}
+      </p>
+    </div>
+    <span className="address">
+      {product.address.state_name}
+    </span>
+  </article>
+)
+
+Product.propTypes = {
+  product: PropTypes.object.isRequired
 }
 
 export default Product
