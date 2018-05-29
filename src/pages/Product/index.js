bbf11b7c42b75c9e6a6db2d225cfe49d8a92b5e5
Guilherme dos Reis <greis.gnr@gmail.com>
2018-05-29 09:21:29 -0300

    start the structure of product page component

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -1,9 +1,53 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export class Product extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      product: null
+    }
+
+    this.id = props.match.params.id
+    this.getProduct()
+  }
+
+  getProduct() {
+    const url = `https://api.mercadolibre.com/items/${this.id}`
+
+    axios.get(url).then(response => {
+      const product = response.data
+      this.setState({product})
+    })
+  }
+
   render() {
+    const { product } = this.state;
     return (
-      <h1>Product page</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="product-card">
+              { !!product && 
+                <div className="row">
+                  <div className="col-8">
+                    <figure>
+                      <img src={product.pictures[0].secure_url} alt={product.title} />
+                    </figure>
+                  </div>
+                  <div className="col-4">
+                    <p className="product-status">
+                      {product.condition} - {product.sold_quantity} selled
+                    </p>
+                    <h1 className="product-name">{product.title}</h1>
+                  </div>
+                </div>
+              }
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
