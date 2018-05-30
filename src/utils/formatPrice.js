0f85f04d7b9e7d4140ddfd8bd3165d0c08624f7c
Guilherme dos Reis <greis.gnr@gmail.com>
2018-05-29 22:38:50 -0300

    create form price cents helper function

--- src/utils/formatPrice.js
+++ src/utils/formatPrice.js
@@ -1,6 +1,11 @@
-const formatPrice = (price) => {
+export const formatPrice = (price) => {
   const str = price.toString()
-  return str.replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+  const arr = str.split('.')
+  return arr.shift().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
 }
 
-export default formatPrice
+export const formatPriceCents = (price, zeroCents = false) => {
+  const str = price.toString()
+  const arr = str.split('.')
+  return arr.length === 1 ? zeroCents ? '00' : '' : arr.pop()
+}
