12c748aa0d6140121ab69fb39bc83a3b1eb350c3
Guilherme dos Reis <greis.gnr@gmail.com>
2018-05-29 23:13:09 -0300

    Move html of product details to product card component

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 
-import {formatPrice, formatPriceCents} from '../../utils/formatPrice'
 import Breadcumb from '../../core/Breadcumb'
-import Button from '../../core/Button'
+import ProductCard from '../../core/ProductCard'
 
 export class Product extends Component {
   constructor(props) {
@@ -21,12 +20,10 @@ export class Product extends Component {
   async getProduct() {
     const url = `https://api.mercadolibre.com/items/${this.id}`
     const urlDescription = `https://api.mercadolibre.com/items/${this.id}/description`
-    let productResp
-    let descriptionResp
 
     try {
-      productResp = await axios.get(url)
-      descriptionResp = await axios.get(urlDescription)
+      const productResp = await axios.get(url)
+      const descriptionResp = await axios.get(urlDescription)
 
       this.setState({
         product: productResp.data,
@@ -38,40 +35,17 @@ export class Product extends Component {
   }
 
   render() {
-    const { product, description } = this.state;
     return (
       <div className="container">
         <div className="row justify-content-lg-center">
           <div className="col col-lg-10">
             <Breadcumb />
-            <div className="product-card">
-              { !!product && 
-                <div className="row">
-                  <div className="col-8">
-                    <figure>
-                      <img src={product.pictures[0].secure_url} alt={product.title} />
-                    </figure>
-                  </div>
-                  <div className="col-4">
-                    <p className="product-status">
-                      {product.condition} - {product.sold_quantity} vendidos
-                    </p>
-                    <h1 className="product-name">{product.title}</h1>
-                    <h2 className="product-price">
-                      $ {formatPrice(product.price)}
-                      <small>{formatPriceCents(product.price, true)}</small>
-                    </h2>
-                    <Button />
-                  </div>
-                  <div className="col-12">
-                    <h3>Descripción del produto</h3>
-                    {description.plain_text.split("\n").map((item, i) => (
-                      <p className="mb-0" key={i}>{item}</p>
-                    ))}
-                  </div>
-                </div>
-              }
-            </div>
+            { !!this.state.product && 
+              <ProductCard
+                product={this.state.product}
+                description={this.state.description}
+              />
+            }
           </div>
         </div>
       </div>
