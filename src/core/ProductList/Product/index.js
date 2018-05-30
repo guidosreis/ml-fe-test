3455535cd54e74c7a5b805de9df7ce340d89a19f
Guilherme dos Reis <greis.gnr@gmail.com>
2018-05-30 07:47:47 -0300

    add and use Link component in product list item component

--- src/core/ProductList/Product/index.js
+++ src/core/ProductList/Product/index.js
@@ -1,26 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 import './styles.css'
 import {formatPrice} from '../../../utils/formatPrice'
 
 const Product = ({ product }) => (
-  <article className="product">
-    <figure className="product__thumb">
-      <img src={product.thumbnail} alt={product.title} />
-    </figure>
-    <div className="product__info">
-      <span className="product__info__price">
-        $ {formatPrice(product.price)}
+  <Link to={`product/${product.id}`} className="product-link">
+    <article className="product">
+      <figure className="product__thumb">
+        <img src={product.thumbnail} alt={product.title} />
+      </figure>
+      <div className="product__info">
+        <span className="product__info__price">
+          $ {formatPrice(product.price)}
+        </span>
+        <p className="product__info__title">
+          {product.title}
+        </p>
+      </div>
+      <span className="address">
+        {product.address.state_name}
       </span>
-      <p className="product__info__title">
-        {product.title}
-      </p>
-    </div>
-    <span className="address">
-      {product.address.state_name}
-    </span>
-  </article>
+    </article>
+  </Link>
 )
 
 Product.propTypes = {
