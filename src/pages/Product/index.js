28ae359834e9ca4bcee427ec824528154eee2997
Guilherme dos Reis <greis.gnr@gmail.com>
2018-05-29 22:39:53 -0300

    Add description in product page component

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -1,29 +1,42 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 
+import {formatPrice, formatPriceCents} from '../../utils/formatPrice'
+
 export class Product extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      product: null
+      product: null,
+      description: null
     }
 
     this.id = props.match.params.id
     this.getProduct()
   }
 
-  getProduct() {
+  async getProduct() {
     const url = `https://api.mercadolibre.com/items/${this.id}`
+    const urlDescription = `https://api.mercadolibre.com/items/${this.id}/description`
+    let productResp
+    let descriptionResp
 
-    axios.get(url).then(response => {
-      const product = response.data
-      this.setState({product})
-    })
+    try {
+      productResp = await axios.get(url)
+      descriptionResp = await axios.get(urlDescription)
+
+      this.setState({
+        product: productResp.data,
+        description: descriptionResp.data
+      })
+    } catch (error) {
+      // tratar erro
+    }
   }
 
   render() {
-    const { product } = this.state;
+    const { product, description } = this.state;
     return (
       <div className="container">
         <div className="row">
@@ -38,9 +51,19 @@ export class Product extends Component {
                   </div>
                   <div className="col-4">
                     <p className="product-status">
-                      {product.condition} - {product.sold_quantity} selled
+                      {product.condition} - {product.sold_quantity} vendidos
                     </p>
                     <h1 className="product-name">{product.title}</h1>
+                    <h2 className="product-price">
+                      $ {formatPrice(product.price)}
+                      <small>{formatPriceCents(product.price, true)}</small>
+                    </h2>
+                  </div>
+                  <div className="col-12">
+                    <h3>Descripción del produto</h3>
+                    {description.plain_text.split("\n").map((item, i) => (
+                      <p className="mb-0" key={i}>{item}</p>
+                    ))}
                   </div>
                 </div>
               }
